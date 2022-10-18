9bb67efcd87f18df86b8aa0975552c3593a0fd97
yakubu-josiah <jossysmothest007@gmail.com>
2022-10-18 12:08:32 +0100

    Reset icon for retrieving account added

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <x-HomeComps.regForm>
         <div class="card-body ">
-            <div class="card-header"><i class="feather icon-user auth-icon"></i> {{ __('Reset Password') }}</div>
+            <div class="card-header"><i class="feather icon-lock auth-icon mb-0"></i> {{ __('Reset Password') }}</div>
 
             <div class="card-body">
                 @if (session('status'))
@@ -16,7 +16,7 @@
                     @csrf
 
                     <div class="row mb-6">
-                        <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                        <label for="email" class="col-md-6 col-form-label text-md-end mb-2">{{ __('Email Address') }}</label>
 
                         <div class="col-md-12">
                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -29,9 +29,9 @@
                         </div>
                     </div>
 
-                    <div class="form-group text-left">
+                    <div class="form-group text-left ">
                         <div class="col-md-6 mt-4">
-                            <button type="submit" class="btn btn-primary shadow-2 mb-4">
+                            <button type="submit" class="btn btn-info shadow-2">
                                 {{ __('Send Password Reset Link') }}
                             </button>
                         </div>
