d6d0bf9c51f0a067241e9d616a21b0534451097f
yakubu-josiah <jossysmothest007@gmail.com>
2022-10-24 13:10:57 +0100

    Migrating all User and Admin Auctioning models functions

---
+++ app/Models/UserSettings.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserSettings extends Model
+{
+    protected $fillable = 
+    [
+        'id',
+        'user_id',
+        'account_name',
+        'accoumt_number',
+        'bank_name',
+        'account_type'
+    ];
+    use HasFactory;
+}
